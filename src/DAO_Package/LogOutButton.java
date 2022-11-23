66d7262a26c8bc504d1ccd574c4575fc8c19e874
HieuDoNguyenTrong <thienthan726@gmail.com>
2022-11-23 20:48:14 +0700

    done customer

---
+++ src/DAO_Package/LogOutButton.java
@@ -0,0 +1,13 @@
+package DAO_Package;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LogOutButton {
+    @FXML
+
+    public void logOutButton(ActionEvent event) throws Exception{
+
+
+    }
+}
